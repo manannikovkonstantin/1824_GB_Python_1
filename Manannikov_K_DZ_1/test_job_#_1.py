ff163e2546dff178fc4d6023cdb4ff08df6c1900
Konstantin <k.manannikov@gmail.com>
2021-10-01 17:28:59 +0300

    сделал 1 задание

---
+++ Manannikov_K_DZ_1/test_job_#_1.py
@@ -0,0 +1,17 @@
+duration = int(input('Введите кол-во секунд'))
+
+day = duration % 29030400 % 2419200 % 604800 // 86400
+hour = duration % 29030400 % 2419200 % 604800 % 86400 // 3600
+minutes = duration % 29030400 % 2419200 % 604800 % 86400 % 3600 // 60
+seconds = duration % 29030400 % 2419200 % 604800 % 86400 % 3600 % 60
+
+if day >= 0:
+    print("{} дн {} час {} мин {} сек.".format(day,hour,minutes,seconds))
+elif hour >= 0:
+    print("{} час {} мин {} сек.".format(day, hour, minutes, seconds))
+elif minutes >= 0:
+    print("{} мин {} сек.".format(day, hour, minutes, seconds))
+else:
+    print("{} сек.".format(seconds))
+
+
