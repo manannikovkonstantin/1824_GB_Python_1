7150bd18678e58c97553ffde805ad2f296f529c4
Konstantin <k.manannikov@gmail.com>
2021-10-01 20:11:49 +0300

    сделал 2 задание a. b.

---
+++ Manannikov_K_DZ_1/test_job_#2.py
@@ -0,0 +1,14 @@
+i = 0
+while i <= 999:
+    num = i % 2
+    if num != 0:
+        degree = [int(a) for a in str(i ** 3)]
+        sum = 0
+        for q in degree:
+            sum += q + 17
+        del_7 = sum % 7
+        if del_7 == 0:
+            print(i)
+    i += 1
+
+
