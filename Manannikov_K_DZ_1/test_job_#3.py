d9cfb5b2fc281e2da52d924144d75b8d1870e9ff
Konstantin <k.manannikov@gmail.com>
2021-10-04 02:20:45 +0300

    допилил 3

--- Manannikov_K_DZ_1/test_job_#3.py
+++ Manannikov_K_DZ_1/test_job_#3.py
@@ -1,23 +1,13 @@
-num = 0
 percentage = 0
-i = 0
-while i <= 20:
-    if 1 == i:
-        percentage = 'процент'
-    elif 2 <= i < 5:
-        percentage = 'процента'
-    elif 5 <= i or i == 0:
+for i in range(1,101):
+    num = str(i)
+    num_11 = int(num)
+    num = int(num[-1])
+    if 5 <= num <= 9 or 11 <= num_11 <= 20:
         percentage = 'процентов'
+    elif 2 <= num <= 4:
+        percentage = 'процента'
+    elif num == 1:
+        percentage = 'процент'
+
     print("{} {}".format(i,percentage))
-    i += 1
-
-
-
-
-
-
-
-
-
-
-
