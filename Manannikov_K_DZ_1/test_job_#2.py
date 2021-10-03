c5736fc68fcc84a04dc9a1715e114a150aeb0b1d
Konstantin <k.manannikov@gmail.com>
2021-10-04 02:20:39 +0300

    допилил 2

--- Manannikov_K_DZ_1/test_job_#2.py
+++ Manannikov_K_DZ_1/test_job_#2.py
@@ -1,12 +1,17 @@
-i = 0
-while i <= 999:
-    num = i % 2
-    if num != 0:
-        degree = [int(a) for a in str(i ** 3)]
-        sum = 0
-        for q in degree:
-            sum += q + 17
-        del_7 = sum % 7
+def function(value):
+    degree = []
+    sum_numers = 0
+    for i in range(1, 1000, 2):
+        degree.append(i ** 3 + value)
+    for item in degree:
+        str_degree = str(item)
+        pred_sum = 0
+        for x in str_degree:
+            int_degree = int(x)
+            pred_sum += int_degree
+            del_7 = pred_sum % 7
         if del_7 == 0:
-            print(i)
-    i += 1
+            sum_numers += int(str_degree)
+    print(sum_numers)
+function(0)
+function(17)
